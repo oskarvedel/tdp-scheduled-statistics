815b547e9e1e4993c12f82208a659680d3f84438
oskarvedel <oskar.vedel@gmail.com>
2023-12-11 17:12:57 +0100

    smallfix

--- scheduled-statistics-calcs-per-geolocation.php
+++ scheduled-statistics-calcs-per-geolocation.php
@@ -180,7 +180,6 @@ add_shortcode("update_gd_place_list_for_geolocation", "update_gd_place_list_for_
 function update_statistics_data_for_all_geolocations()
 {
     $geolocations = get_posts(array('post_type' => 'geolocations', 'posts_per_page' => -1));
-    $geolocations = get_posts(array('post_type' => 'geolocations', 'p' => 6345));
     foreach ($geolocations as $geolocation) {
         $geolocation_id = $geolocation->ID;
         $gd_place_ids_list = get_post_meta($geolocation_id, 'gd_place_list', false);
