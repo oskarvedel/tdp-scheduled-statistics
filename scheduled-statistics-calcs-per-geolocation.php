3122db761eaa19dd72e0db9870c4d63242aea010
oskarvedel <oskar.vedel@gmail.com>
2023-12-11 17:07:09 +0100

    debugged no lowest prices

--- scheduled-statistics-calcs-per-geolocation.php
+++ scheduled-statistics-calcs-per-geolocation.php
@@ -49,26 +49,25 @@ function get_statistics_data_for_list_of_gd_places($gd_place_ids_list)
         $statistics_data_for_single_gd_place = get_statistics_data_for_single_gd_place_statistics($gd_place_id);
         // Check if depotrum data is available for the gd_place
         if ($statistics_data_for_single_gd_place) {
+            $counter++;
             // Add depotrum data to the existing statistics data
             foreach ($statistics_data_for_single_gd_place as $field => $value) {
                 $value = floatval($value); //convert value to float
-                if ($value !== 0 && $value !== null) {
+                if ($value != 0 && $value !== null) {
                     if (strpos($field, 'smallest') !== false || strpos($field, 'largest') !== false) {
                         $statistics_data[$field] = find_smallest_or_largest_m2_size_per_geolocation($field, $value, $statistics_data);
                     } else if (strpos($field, 'lowest') !== false || strpos($field, 'highest') !== false) {
                         $statistics_data[$field] = find_lowest_or_highest_price_per_geolocation($field, $value, $statistics_data);
-                        if (strpos($field, 'lowest')  !== false) {
-                            //trigger_error("updating lowest price field: " . $field . " with value: " . $value, E_USER_WARNING);
-                        }
                     } else {
                         $statistics_data[$field] = add_fields($field, $value, $statistics_data);
                         //trigger_error("updarting non-smallestorlargest field: " . $field . " value: " . $value, E_USER_WARNING);
                     }
                 }
             }
-            $counter++;
         }
     }
+
+    xdebug_break();
 
     // Calculate averages
     foreach ($statistics_data as $field => $value) {
@@ -79,7 +78,7 @@ function get_statistics_data_for_list_of_gd_places($gd_place_ids_list)
             round($statistics_data[$field] = $value / $counter, 2);
         }
     }
-
+    xdebug_break();
     return $statistics_data;
 }
 
@@ -88,7 +87,7 @@ function add_fields($field, $value, $statistics_data)
     if (!is_numeric($value)) {
         trigger_error("field: " . $field . " value: " . $value . " is not numeric. statistics_data var_dump: " . var_dump($statistics_data), E_USER_WARNING);
     }
-    if (isset($statistics_data[$field])) {
+    if (array_key_exists($field, $statistics_data)) {
         return $statistics_data[$field] += $value;
     } else {
         return  $value;
@@ -97,30 +96,31 @@ function add_fields($field, $value, $statistics_data)
 
 function find_smallest_or_largest_m2_size_per_geolocation($field, $value, $statistics_data)
 {
-    if (!isset($statistics_data[$field])) {
+    if (!array_key_exists($field, $statistics_data)) {
         return $value;
     }
 
-    if ((strpos($field, 'smallest') && $value < $statistics_data[$field]) || (strpos($field, 'largest') && $value > $statistics_data[$field])) {
+    if ((strpos($field, 'smallest') !== false  && $value < $statistics_data[$field]) || (strpos($field, 'largest')  !== false  && $value > $statistics_data[$field])) {
         return $value;
     } else {
         return $statistics_data[$field];
     }
 }
 
-function find_lowest_or_highest_price_per_geolocation($field, $value, $statistics_data)
+function find_lowest_or_highest_price_per_geolocation($field, $new_value, $statistics_data)
 {
-    if (!isset($statistics_data[$field])) {
+    if (!array_key_exists($field, $statistics_data)) {
         //trigger_error("value not set encountered in geolocations statistics-calcs. field: " . $field . " value: " . $value, E_USER_WARNING);
-        return $value;
+        return $new_value;
     }
+    $existing_value = $statistics_data[$field];
 
-    if ((strpos($field, 'lowest') && $value < $statistics_data[$field]) || (strpos($field, 'highest') && $value > $statistics_data[$field])) {
+    if ((strpos($field, 'lowest')  !== false  && $new_value < $existing_value) || (strpos($field, 'highest')  !== false && $new_value > $existing_value)) {
         //trigger_error("field: " . $field . " value: " . $value ."higher or lower than " . $statistics_data[$field] . " returning value", E_USER_WARNING);
-        return $value;
+        return $new_value;
     } else {
         //trigger_error("field: " . $field . " value: " . $value ."NOT higher or lower than " . $statistics_data[$field] . " returning original field", E_USER_WARNING);
-        return $statistics_data[$field];
+        return $existing_value;
     }
 }
 
@@ -180,29 +180,17 @@ add_shortcode("update_gd_place_list_for_geolocation", "update_gd_place_list_for_
 function update_statistics_data_for_all_geolocations()
 {
     $geolocations = get_posts(array('post_type' => 'geolocations', 'posts_per_page' => -1));
-
+    $geolocations = get_posts(array('post_type' => 'geolocations', 'p' => 6345));
     foreach ($geolocations as $geolocation) {
         $geolocation_id = $geolocation->ID;
-        $message = "updating data for geolocation: " . $geolocation->post_name . " with id: " . $geolocation_id . "\n";
-        //trigger_error("updating data for geolocation: " . $geolocation->post_name, E_USER_WARNING);
-
         $gd_place_ids_list = get_post_meta($geolocation_id, 'gd_place_list', false);
-
-        $message .= "gd_place_list var_dump: \n";
-        foreach ($gd_place_list as $gd_place) {
-            $message .= "gd_place_list element: " . $gd_place . "\n";
-        }
-        $message .= "gd_place_list var_dump:" . var_dump($gd_place_list) . "\n";
-        $message .= "gd_place_list print_r:" . print_r($gd_place_list) . "\n";
-
-        trigger_error($message, E_USER_WARNING);
 
         $depotrum_data = get_statistics_data_for_list_of_gd_places($gd_place_ids_list);
         //trigger_error("depotrum_data var_dump:" . var_dump($depotrum_data), E_USER_WARNING);
         foreach ($depotrum_data as $field => $value) {
             update_post_meta($geolocation_id, $field, $value);
             if (strpos($field, 'lowest')  !== false) {
-                //trigger_error("updating lowest price field: " . $field . " with value: " . $value .  "for geolocation "  . $geolocation->post_name, E_USER_WARNING);
+                trigger_error("updating lowest price field: " . $field . " with value: " . $value .  "for geolocation "  . $geolocation->post_name, E_USER_WARNING);
             }
             //trigger_error("updated field: " . $field . " with value: " . $value . "for geolocation" . $geolocation->post_name , E_USER_WARNING);
         }
